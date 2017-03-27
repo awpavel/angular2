e9ff450372e07b945e4817f01e6501ad2d3fe29a
admin <aw.pavel@gmail.com>
2017-03-27 15:29:21 +0300

    Initial commit

---
+++ src/app/app.component.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
+    }
+    AppComponent.prototype.onSelected = function (hero) {
+        this.selectedHero = hero;
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n    <h1>{{title}}</h1>\n    <p>Heroes:</p>\n    <ul>\n        <li [class.selected]=\"hero === selectedHero\" *ngFor=\"let hero of heroes\" (click)=\"onSelected(hero)\"  class=\"items\">\n            {{ hero.name }}\n        </li>\n    </ul>\n    <hero-detail [hero]=\"selectedHero\"></hero-detail>\n    ",
+        styles: ["\n        .items:hover {\n            text-decoration: underline;\n            cursor: pointer;\n        }\n        .items {\n            text-decoration: none;\n        }\n        .selected {\n            background-color: #CFD8DC !important;\n            color: white;\n        }        \n    "]
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
